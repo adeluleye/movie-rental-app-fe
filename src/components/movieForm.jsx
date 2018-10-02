985ab9d326b0fddc2a1b86ab9d6b26ac1f048563
adeluleye <adelu4real@gmail.com>
2018-10-02 16:02:07 +0100

    implement save and programmatic navigation from movieForm to moviesTable

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MovieForm = ({ match }) => {
-  return <h1>MovieForm {match.params.id}</h1>;
-};
+class MovieForm extends Component {
+  handleSave = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <h1>MovieForm {match.params.id}</h1>
+        <button onClick={this.handleSave} className="btn btn-primary btn-sm">
+          Save
+        </button>
+      </div>
+    );
+  }
+}
 
 export default MovieForm;
