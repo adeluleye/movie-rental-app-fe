2351e2d1ef1476ff43915aa4bfcd8ac37d0cee53
adeluleye <adelu4real@gmail.com>
2018-10-02 17:59:57 +0100

    add login link to navbar and route to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Customers from './components/customers';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
 import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
 import './App.css';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
+            <Route path="/login" component={LoginForm} />
             <Route path="/not-found" component={NotFound} />
             <Redirect exact from="/" to="/movies" />
             <Redirect to="/not-found" />
