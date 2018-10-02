40929c9bc296c83c1f321bebd908fa3c3dfbcfa4
adeluleye <adelu4real@gmail.com>
2018-10-02 15:25:42 +0100

    add navBar component

---
+++ src/components/navBar.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        MRA
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/movies">
+              Movies <span className="sr-only">(current)</span>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/customers">
+              Customers
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/rentals">
+              Rentals
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <NavLink className="navbar-brand" to="#">
+    Navbar
+  </NavLink>
+  <button
+    className="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarNav"
+    aria-controls="navbarNav"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span className="navbar-toggler-icon" />
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <NavLink className="nav-link" to="#">
+          Home <span className="sr-only">(current)</span>
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="#">
+          Features
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="#">
+          Pricing
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link disabled" to="#">
+          Disabled
+        </NavLink>
+      </li>
+    </ul>
+  </div>
+</nav>;
