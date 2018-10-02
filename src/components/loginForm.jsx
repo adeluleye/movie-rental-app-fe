9d3769dedb883fa33be4658c77ee04eb54c81b9f
adeluleye <adelu4real@gmail.com>
2018-10-02 18:40:36 +0100

    controlled username element to have single source of truth

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
-  username = React.createRef();
-
-  //   componentDidMount() {
-  //     this.username.current.focus();
-  //   }
+  state = {
+    account: { username: '', password: '' }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
@@ -13,6 +11,12 @@ class LoginForm extends Component {
     // Call the server
 
     console.log('Submitted');
+  };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
   };
 
   render() {
@@ -24,6 +28,8 @@ class LoginForm extends Component {
             <label htmlFor="username">Username</label>
             <input
               autoFocus
+              value={this.state.account.username}
+              onChange={this.handleChange}
               id="username"
               type="text"
               className="form-control"
