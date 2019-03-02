b35424ad16c791437adfdfcaaa3f874c48a1d399
adeluleye <adelu4real@gmail.com>
2019-03-02 23:06:05 +0100

    dynamically use api url for production and dev

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,8 @@
 import http from './httpService';
-import { apiUrl } from '../config.json';
+//import { apiUrl } from '../config.json';
 
-const apiEndPoint = apiUrl + "/users";
+//const apiEndPoint = apiUrl + "/users";
+const apiEndPoint = "/users";
 
 export function register(user) {
     return http.post(apiEndPoint, {
